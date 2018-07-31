005707f4b9bfda1f443b81cbceefd804a50fe82e
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-07-31 12:50:40 +0530

    Create greater.java

---
+++ greater.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		if(a>b&&a>c)
+		{
+			System.out.println("a greater");
+			}
+			else if(b>a&&b>c)
+			{
+				System.out.println("b greater");
+				}
+				else
+				{
+					System.out.println("c greater");
+				}
+	}
+}
