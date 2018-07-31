6229115ccbb27566536d94be78b6944a7f26a794
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-07-31 11:18:42 +0530

    Create con.java

---
+++ con.java
@@ -0,0 +1,43 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		  char n;
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter the character you want to check:");
+        n = (char) bf.read();
+        switch(n)
+        {
+            case 'a':
+            System.out.println(" vowel");
+            break;
+ 
+            case 'e':
+            System.out.println(" vowel");
+            break;
+ 
+            case 'i':
+            System.out.println(" vowel");
+            break;
+ 
+            case 'o':
+            System.out.println(" vowel");
+            break;
+               case 'u':
+            System.out.println(" vowel");
+            break;
+ 
+            default:
+            System.out.println(" consonant");
+            break;
+        
+    }
+	}
+}
