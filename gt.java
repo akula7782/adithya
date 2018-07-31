d21ec068f1880225dff83b022fcf31bd311fb05a
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-07-31 12:57:25 +0530

    Create gt.java

---
+++ gt.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b,c;
+	Scanner sc=new Scanner(System.in);
+		 a=sc.nextInt();
+		 b=sc.nextInt();
+		 c=sc.nextInt();
+		if(a>b&&a>c)
+		{
+			System.out.println("a greater");
+			}
+			else if(b>a&&b>c)
+			{
+				System.out.println("b greater");
+				}
+				else
+				{
+					System.out.println("c greater");
+				}
+	}
+}
