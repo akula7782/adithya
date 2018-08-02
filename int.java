d72375b1fcc7989786aa6866935e098e997b622f
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-08-02 16:17:35 +0530

    Create int.java

---
+++ int.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int n,i
+         
+        Scanner s = new Scanner(System.in);
+         int k=s.nextInt();
+          n=s.nextInt();
+            for(i=k+1; i<n; i++)
+        {
+            if(i%2!=0)
+                System.out.print(i+ " ");
+        }   
+	}
+}
