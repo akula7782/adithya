9d1dbec920e71dc7b9147b6b83f23e4832b6e81f
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-08-06 15:49:29 +0530

    Update pme.java

--- pme.java
+++ pme.java
@@ -15,7 +15,7 @@ class Ideone
 		  while (low < high) {
             boolean flag = false;
 
-            for(int i = 2; i <= low/2; ++i) {
+            for(int i = 2; i <= low/2; i++) {
                  if(low % i == 0) {
                     flag = true;
                     break;
