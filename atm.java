93b71a5ca5aa9b5706aa42c8e3f85797d6884400
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-08-08 19:00:17 +0530

    Update and rename arthe.java to atm.java

--- atm.java
+++ atm.java
@@ -3,18 +3,16 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	      	int a,d,n,sn=0;
+	      	int a,d,n,s=0;
 		Scanner sc=new Scanner(System.in);
 		n=sc.nextInt();
 		a=sc.nextInt();
 		d=sc.nextInt();
-		sn=(n*(2*a+(n-1)*d))/2;
+		s=(n*(2*a+(n-1)*d))/2;
 		System.out.println(sn+ " ");
 	}
 }
