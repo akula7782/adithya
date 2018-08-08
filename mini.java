08e279dfc54b9474fd1b8347dad3afc732334829
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-08-08 19:08:11 +0530

    Update mini.java

--- mini.java
+++ mini.java
@@ -1,21 +1,17 @@
-/* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		int n, mini;
-        Scanner s = new Scanner(System.in);
-         n = s.nextInt();
+        Scanner sc = new Scanner(System.in);
+         n = sc.nextInt();
         int a[] = new int[n];
         for(int i = 0; i < n; i++)
         {
-            a[i] = s.nextInt();
+            a[i] = sc.nextInt();
         }
         mini = a[0];
         for(int i = 1; i < n; i++)
