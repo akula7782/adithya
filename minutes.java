c12d30c70d12202dbda2905baf9245d88a4a9603
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-08-09 12:25:41 +0530

    Create minutes.java

---
+++ minutes.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int t;
+		Scanner sc=new Scanner(System.in);
+		t=sc.nextInt();
+		int h=t/60;
+		int m=t%60;
+		System.out.println(h+ " " +m+ " ");
+	}
+}
