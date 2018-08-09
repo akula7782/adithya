91726a4036050158b9c91b41f9412166e9ed8b30
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-08-09 10:47:02 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		String str=sc.next();
+			char c;
+		int j;
+		int flag=0;
+		for(j=0;j<str.length();j++)
+		{
+			c=str.charAt(j);
+			if(Character.isLetter(c))
+			{
+				flag=1;
+			}
+		}
+		if(flag==1)
+			System.out.println("no");
+		else
+		System.out.println("Yes");
+	}
+}
