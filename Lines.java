63be16ca772a6d29a874f648ce5a886ef9c9d92e
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-08-14 11:33:59 +0530

    Create Lines.java

---
+++ Lines.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int lines=1;
+		Scanner sc = new Scanner(System.in);
+		String str=sc.nextLine();
+		while(sc.hasNextLine())
+                        {  
+                        sc.nextLine();
+                           lines++;
+                        }
+                         System.out.println(lines);
+	}
+}
