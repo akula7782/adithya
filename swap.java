8680903f33cec15d5fd8f731e193e846f8e634bf
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-08-20 11:48:50 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int temp;
+	   Scanner sc=new Scanner(System.in);
+	   int a=sc.nextInt();
+	   int b=sc.nextInt();
+	   temp=a;
+	   a=b;
+	   b=temp;
+	   System.out.println(a+ " "+b+ " ");
+	   
+	}
+}
