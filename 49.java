c4c7d33f62384524d43751f487043d56ed6abdab
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-08-25 12:02:03 +0530

    Create 49.java

---
+++ 49.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int sum=a+b;
+		System.out.println(sum);
+		}
+}
