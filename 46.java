283aaf7d3f4206fffe2219c61338a6ba81c5939b
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-08-25 10:06:47 +0530

    Create 46.java

---
+++ 46.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner sc = new Scanner(System.in);
+		n=sc.nextInt();
+		if(n>0)
+		{
+			n=n+1;
+			System.out.println(n);
+			}
+			}
+}
