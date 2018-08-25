83263c8bcfaa5060c561481b0992a78ec4199a22
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-08-25 15:46:19 +0530

    Create 51.java

---
+++ 51.java
@@ -0,0 +1,21 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int t,k,m,l,b;
+		Scanner sc=new Scanner(System.in);
+		t=sc.nextInt();
+	
+	k=t%10;
+	m=t/10;
+	l=m%10;
+	b=m/10;
+System.out.println(b+" "+l+" "+k);
+
+	}
+}
