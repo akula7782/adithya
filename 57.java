e17bd4d1b09bea2baea3dd2d3a2e1bdfbcdbe59b
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-09-07 22:18:53 +0530

    Create 57.java

---
+++ 57.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 int n,k,c=0;
+              Scanner sc=new Scanner(System.in);
+              n=sc.nextInt();
+              k=sc.nextInt();
+              int a[]=new int[20];
+              for(int i=0;i<n;i++)
+              {
+           	 a[i]=sc.nextInt();
+              if(a[i]==k)
+              {
+              	c++;
+              }
+              }
+            System.out.println(c);  
+	}
+}
