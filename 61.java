b806b0caba4a41afd235a6665def5d1b5766e75f
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-09-08 12:27:27 +0530

    Create 61.java

---
+++ 61.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str;
+		char st;
+		int n;
+		Scanner sc=new Scanner(System.in);
+		str=sc.next();
+		n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+	             System.out.print(str.charAt(i));
+		}
+}
+}
