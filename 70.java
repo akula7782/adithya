0e1db809db25ed172e6d71e40e048e3882996c43
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-09-10 12:39:51 +0530

    Create 70.java

---
+++ 70.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			int c,r;
+		Scanner sc=new Scanner(System.in);
+		 c=sc.nextInt();
+		 r=c*2;
+		 System.out.println(r);
+	}
+}
