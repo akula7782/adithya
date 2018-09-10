a01472df0c0b37d006a331e8393d0158d062ac03
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-09-10 12:28:56 +0530

    Create 69.java

---
+++ 69.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int c;
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		c=a-b;
+		if(c%2==0)
+		System.out.println("even");
+		else
+			System.out.println("odd");
+		}
+}
