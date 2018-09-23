20b1e3fd1dc916673e6cbf7338e861d9572736f2
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-09-23 11:04:09 +0530

    Create 74.java

---
+++ 74.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		float i;
+		Scanner sc=new Scanner(System.in);
+		i=sc.nextFloat();
+		System.out.println(Math.round(i));
+	}
+}
