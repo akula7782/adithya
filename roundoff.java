ae7b49d254c827ec2634839ec13e281e79fa9c1c
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-09-23 11:16:37 +0530

    Create roundoff.java

---
+++ roundoff.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		float a =sc.nextFloat();;
+
+int d = (int) Math.ceil(a);
+
+System.out.println(d);
+	}
+}
