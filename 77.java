844b3a00d91cb7046b53fdd91991a8a158fe22c1
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-09-28 13:30:10 +0530

    Create 77.java

---
+++ 77.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n1;
+          Scanner sc=new Scanner(System.in);
+          n1=sc.nextInt();
+
+        
+        for(int i = 1; i <= n1; ++i) {
+            if (n1 % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+	}
+}
