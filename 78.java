6432bcca042644efb6f1ec03ae73eed7a800b523
akula7782 <41738424+akula7782@users.noreply.github.com>
2018-09-28 13:41:39 +0530

    Create 78.java

---
+++ 78.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n%13==0)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+		}
+}
